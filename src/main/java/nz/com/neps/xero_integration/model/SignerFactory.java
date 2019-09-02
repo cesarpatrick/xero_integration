fd87018c995ba557926de54e5cebc907e354a3a1
Cesar Medeiros <cesar.doublex@gmail.com>
2019-09-02 08:01:57 -0300

    Delete All

--- src/main/java/nz/com/neps/xero_integration/model/SignerFactory.java
+++
@@ -1,7 +0,0 @@
-package nz.com.neps.xero_integration.model;
-
-import com.google.api.client.auth.oauth.OAuthSigner;
-
-public interface SignerFactory {
-	OAuthSigner createSigner(String tokenSharedSecret);
-}
